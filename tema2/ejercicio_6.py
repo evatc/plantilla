8c415eb5ab7c841864c3b47db9a8667e61037506
elenagarcia05 <157798696+elenagarcia05@users.noreply.github.com>
2025-03-08 13:49:06 +0100

    Update ejercicio_6.py

--- tema2/ejercicio_6.py
+++ tema2/ejercicio_6.py
@@ -32,3 +32,48 @@ def voraz(candidatos :list) -> list:
                 lista[comp[1]] = c
 
     return pistas
+
+
+
+
+
+
+
+
+
+
+
+
+import random
+
+def robot(arrayb :list, arrayc :list) -> any:
+    listar = []
+    recursiva(arrayb, arrayc, listar)
+    return listar
+
+
+def recursiva(arrayb :list, arrayc :list,arrayr :list) -> any:
+    listaizqb = []
+    listadchab = []
+    listaizqc = []
+    listadchac = []
+    if(len(arrayb) != 0):
+        pivote = arrayb[0]      
+        for corcho in arrayc:
+            if pivote>corcho:
+                listaizqc.append(corcho)
+            elif pivote<corcho:
+                listadchac.append(corcho)
+            else:
+                arrayr.append([corcho,pivote])
+
+        pivote = arrayr[-1][0]  #Coge el mismo pivote que en lo anterior, pero coge el corcho para compararlo con las botellas
+        for botella in arrayb:
+            if pivote>botella:
+                listaizqb.append(botella)
+            elif pivote<botella:
+                listadchab.append(botella)
+        recursiva(listaizqb,listaizqc,arrayr)
+        recursiva(listadchab,listadchac,arrayr)
+
+    return arrayr
