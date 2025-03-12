a6de4796367890e6f1266f3c2eb06c20aaaa7404
elenagarcia05 <157798696+elenagarcia05@users.noreply.github.com>
2025-03-12 12:22:45 +0100

    Update ejercicio_3.py

--- tema2/ejercicio_3.py
+++ tema2/ejercicio_3.py
@@ -1,9 +1,8 @@
 def mejor_candidato(candidatos :list) -> any:
-    """ Devuelve el mejor candidato """
     return [candidatos[0],candidatos[1]]
 
 def es_completable(solucion :list, candidato :any) -> any:
-    """ Comprueba si se puede llegar a la solución usando ese candidato """
+    # Comprueba si c es mejor candidato que solución,y si lo es, lo cambia 
     if candidato[0] > candidato[1]:
         candidatomax = candidato[0]
         candidatomin = candidato[1]
@@ -19,23 +18,25 @@ def es_completable(solucion :list, candidato :any) -> any:
 def voraz(candidatos :list) -> list:
     solucion = []
     if(len(candidatos) != 0):
-        if(len(candidatos) % 2 == 0):
+        # Si la longitud de la lista es par coge los dos primeros elementos y pone al mayor como máximo y al menor como mínimo
+        if(len(candidatos) % 2 == 0):            
             if candidatos[0] > candidatos[1]:
                 maximo = candidatos[0]
                 minimo = candidatos[1]
             else:
                 maximo = candidatos[1]
                 minimo = candidatos[0]
-            candidatos = candidatos[2:]
+            candidatos = candidatos[2:]  # Borra los dos primeros elementos de la lista candidatos
+        # Si la longitud de la lista es impar y pone el primer elemento de la lista como máximo y como mínimo
         else:
             maximo = candidatos[0]
             minimo = candidatos[0]
-            candidatos = candidatos[1:]
+            candidatos = candidatos[1:] # Borra el primer elemento de la lista candidatos
 
         solucion = [minimo,maximo]
         
         while len(candidatos) > 0:
-            c = mejor_candidato(candidatos)
+            c = mejor_candidato(candidatos) # c es los dos nuevos primeros elementos de la lista candidatos
             candidatos.remove(c[0])
             candidatos.remove(c[1])
             es_completable(solucion, c)
